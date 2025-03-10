69c56fa38a6d96ef8d0e4fb1d8971fe3ad54c250
PauloLafayette <pauloarthurlafayettecosta@gmail.com>
2025-03-10 15:50:20 +0000

    adições de funções recursíveis

--- xadrez.c
+++ xadrez.c
@@ -4,59 +4,76 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
+void  movimentacaoRainha(int rainha){
+    if(rainha>0){
+        movimentacaoRainha(rainha - 1);
+        printf("Esquerda\n");
+    }
+}
+
+void movimentacaoBispo(int bispo){
+    if(bispo>0){
+        
+    int bispo2 = 1, bispo3 = 1;
+    
+    while(bispo2<= 5){//movimentação da peça
+        
+        for(bispo3;bispo3<=bispo2;bispo3++){
+        printf("Cima\n");
+    }
+    
+    printf("Direita\n");
+    bispo2++;
+}
+    movimentacaoBispo(bispo - 1);
+    }
+}
+
+void moviTorre(int torre){
+    if(torre>0){
+        printf("Direita\n");
+        moviTorre(torre - 1);
+    }
+}
 int main() {
     
-    int bispo = 1, bispo2 = 1, rainha = 1, torre = 1,cavalo =1,cavalo2 = 0, escolha;
+    int cavalo =1,cavalo2 = 0, escolha;
     
     printf("Qual peça deseja movimentar ? \n1-Bispo\n2-Rainha\n3-Torre\n4-Cavalo\n Escolha: "); //menu para escolha de peça
     scanf("%d", &escolha);
         
     switch(escolha){ //definir a ação com base na escolha
-        case 1: 
-           
-        while(bispo<= 5){//movimentação da peça
-                for(bispo2;bispo2<=bispo;bispo2++){
-                    printf("Direita\n");
-                }
-                printf("Cima\n");
-            bispo++;
-            }
+        case 1:   
+       movimentacaoBispo(1);
         
-            break;
+    break;
         
         
         case 2:
+        movimentacaoRainha(8);
         
-        do{
-            printf("Esquerda\n");//movimentação da peça
-            rainha++;
-        }while(rainha<= 8);
-        
-        break;
+    break;
         
         case 3:
+        moviTorre(5);
         
-        for(torre;torre<=5;torre++){//movimentação da peça
-            printf("Direita\n");
-        }
-        
-        break;
+    break;
     
         case 4:
-        
+
         for(cavalo;cavalo<=1;cavalo++){//movimentação da peça utilizando estruturas aninhadas
             while(cavalo2<=cavalo){
-                printf("Baixo\n");
+                printf("Cima\n");
                 cavalo2++;
             }
-            printf("Esquerda\n");
+            printf("Direita\n");
         }
-        break;
+    break;
 
-        default://caso a opção seja inválida
+    default://caso a opção seja inválida
         printf("Opção inválida");
         
-    }
+    } 
     
     return 0;
 }
